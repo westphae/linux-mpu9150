3ae59cdce8a09dcce2f81d7e7f5e0c25bf7c602b
Joseph Poirier <jdpoirier@gmail.com>
2015-11-26 21:40:38 -0600

    make and source files for libs, add ai test project

---
+++ libimu.h
@@ -0,0 +1,27 @@
+#ifndef LIBIMU_H
+#define LIBIMU_H
+
+// To avoid having to pass the same command line switches when running
+// the test apps, you can specify the defaults for your platform here.
+
+// RPi I2C bus
+#define DEFAULT_I2C_BUS (1)
+
+// platform independent
+#define DEFAULT_SAMPLE_RATE_HZ	(10)
+#define DEFAULT_YAW_MIX_FACTOR (4)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void close_mpu(void);
+extern int init_mpu(void);
+extern int read_mpu(double *pitch, double *roll, double *heading);
+extern int set_cal(int mag, char *cal_file);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIBIMU_H */
