a28d1866d9d10be2a562a2e608e5b88e969f05f7
Christopher Young <cyoung@gmail.com>
2015-12-24 14:18:57 -0500

    Test program update.

--- test.go
+++ test.go
@@ -3,18 +3,22 @@ package main
 import (
 	"./mpu"
 	"fmt"
+	"time"
 )
 
 func main() {
 	i := mpu.InitMPU()
 	fmt.Printf("%d\n", i)
 	defer mpu.CloseMPU()
+	time.Sleep(98 * time.Millisecond)
 	for {
 		pitch, roll, heading, err := mpu.ReadMPU()
-		if err != nil {
-			fmt.Printf("%s\n", err.Error())
-			continue
+		if err == nil {
+			//			fmt.Printf("%s\n", err.Error())
+			//			time.Sleep(1 * time.Second)
+			//			continue
+			fmt.Printf("%f, %f, %f\n", pitch, roll, heading)
 		}
-		fmt.Printf("%f, %f, %f\n", pitch, roll, heading)
+		time.Sleep(98 * time.Millisecond)
 	}
 }
