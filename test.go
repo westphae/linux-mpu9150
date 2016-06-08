55128b4e118a99811a80356aba1e625015cf16ab
Eric Westphal <eric@dormouse.com>
2016-06-08 18:36:13 -0400

    Add methods to read all MPU data

--- test.go
+++ test.go
@@ -10,18 +10,30 @@ import (
 )
 
 type SituationUpdate struct {
-	pitch   float64
-	roll    float64
-	heading float64
-	p	int16
-	q	int16
-	r	int16
-	x_a	int16
-	y_a	int16
-	z_a	int16
-	mx	int16
-	my	int16
-	mz	int16
+	heading float32
+	pitch   float32
+	roll    float32
+	gx	float32
+	gy	float32
+	gz	float32
+	ax	float32
+	ay	float32
+	az	float32
+	qx	float32
+	qy	float32
+	qz	float32
+	qw	float32
+	mx	float32
+	my	float32
+	mz	float32
+	ts 	uint32
+	tsm 	uint32
+	x_accel float32
+	y_accel float32
+	z_accel float32
+	x_mag float32
+	y_mag float32
+	z_mag float32
 }
 
 var updateChan chan SituationUpdate
@@ -45,31 +57,54 @@ func updateSender(addr string) {
 	// Get updates from the channel, send.
 	for {
 		u := <-updateChan
-		s := fmt.Sprintf("XATTtesting,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f",
-			u.heading, u.pitch, u.roll,
-			float32(u.p)/32767, float32(u.q)/32767, float32(u.r)/32767,
-			float32(u.x_a)/32767, float32(u.y_a)/32767, float32(u.z_a)/32767,
-			float32(u.mx)/32767, float32(u.my)/32767, float32(u.mz)/32767)
+		s := fmt.Sprintf("XATTtesting,%f,%f,%f",
+			u.heading, u.pitch, u.roll)
 		fmt.Printf("%s\n", s)
+		fmt.Printf("gyro: %+6.4f %+6.4f %+6.4f\n",
+			u.gx, u.gy, u.gz)
+		fmt.Printf("acc:  %+6.4f %+6.4f %+6.4f\n",
+			u.ax, u.ay, u.az)
+		fmt.Printf("quat: %+6.4f %+6.4f %+6.4f %+6.4f\n",
+			u.qx, u.qy, u.qz, u.qw)
+		fmt.Printf("mag:  %+6.4f %+6.4f %+6.4f\n",
+			u.mx, u.my, u.mz)
+		fmt.Printf("ts:   %d %d\n", u.ts, u.tsm)
+		fmt.Printf("cacc: %+6.4f %+6.4f %+6.4f\n",
+			u.x_accel, u.y_accel, u.z_accel)
+		fmt.Printf("magc: %+6.4f %+6.4f %+6.4f\n",
+			u.x_mag, u.y_mag, u.z_mag)
+		fmt.Printf("\n")
 		conn.Write([]byte(s))
 	}
 }
 
 // Converts from radian to degrees and sends to update channel.
-func sendUpdate(pitch, roll, heading float32, p, q, r, x_a, y_a, z_a, mx, my, mz int16) {
+func sendUpdate(d mpu.IMUData) {
 	var u SituationUpdate
-	u.pitch = float64(pitch) * (float64(180.0) / math.Pi)
-	u.roll = float64(roll) * (float64(180.0) / math.Pi)
-	u.heading = float64(heading) * (float64(180.0) / math.Pi)
-	u.p = p
-	u.q = q
-	u.r = r
-	u.x_a = x_a
-	u.y_a = y_a
-	u.z_a = z_a
-	u.mx = mx
-	u.my = my
-	u.mz = mz
+	u.pitch = float32(d.Pitch*180.0/math.Pi)
+	u.roll = float32(d.Roll*180.0/math.Pi)
+	u.heading = float32(d.Heading*180.0/math.Pi)
+	u.gx = float32(d.Gx)/32767
+	u.gy = float32(d.Gy)/32767
+	u.gz = float32(d.Gz)/32767
+	u.ax = float32(d.Ax)/32767
+	u.ay = float32(d.Ay)/32767
+	u.az = float32(d.Az)/32767
+	u.qx = float32(d.Qx)/2147483647
+	u.qy = float32(d.Qy)/2147483647
+	u.qz = float32(d.Qz)/2147483647
+	u.qw = float32(d.Qw)/2147483647
+	u.mx = float32(d.Mx)/32767
+	u.my = float32(d.My)/32767
+	u.mz = float32(d.Mz)/32767
+	u.ts = d.Ts
+	u.tsm = d.Tsm
+	u.x_accel = float32(d.X_accel)/32767
+	u.y_accel = float32(d.Y_accel)/32767
+	u.z_accel = float32(d.Z_accel)/32767
+	u.x_mag = float32(d.X_mag)/32767
+	u.y_mag = float32(d.Y_mag)/32767
+	u.z_mag = float32(d.Z_mag)/32767
 	updateChan <- u
 }
 
@@ -84,15 +119,12 @@ func main() {
 	defer mpu.CloseMPU()
 	time.Sleep(98 * time.Millisecond)
 	for {
-		raw, err_r := mpu.ReadMPURaw();
+		d, err_r := mpu.ReadMPUAll();
 		if err_r == nil {
 			//			fmt.Printf("%s\n", err.Error())
 			//			time.Sleep(1 * time.Second)
 			//			continue
-			sendUpdate(
-				// pitch, roll, heading,
-				0, 0, 0,
-				raw.Gx, raw.Gy, raw.Gz, raw.Ax, raw.Ay, raw.Az, raw.Mx, raw.My, raw.Mz)
+			sendUpdate(d)
 		}
 		time.Sleep(98 * time.Millisecond)
 	}
